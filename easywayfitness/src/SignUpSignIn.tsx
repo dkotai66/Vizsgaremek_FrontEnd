aa09c79bb3e8fd5cc955815caffc1e141edd98ad
Kótai Dániel <dkotai66@gmail.com>
2023-02-10 15:00:37 +0100

    regisztracio es belepes login design kesz

--- easywayfitness/src/SignUpSignIn.tsx
+++ easywayfitness/src/SignUpSignIn.tsx
@@ -68,31 +68,67 @@ class SignUpSignIn extends Component<{}, State> {
         });
         await this.usersLoad();
     };
+
+    Registration = async () => {
+        const log = document.getElementById('login') as HTMLFormElement;
+        const reg = document.getElementById('register') as HTMLFormElement;
+        const btn = document.getElementById('invisibleButton') as HTMLButtonElement;
+        const regButn = document.getElementById('regButton') as HTMLButtonElement;
+        const logButn = document.getElementById('logButton') as HTMLButtonElement;
+        reg.style.display = "block"
+        log.style.display = "none"
+        btn.style.left = "0";
+        regButn.style.color = "black";
+        logButn.style.color = "green";
+    }
+
+    Login = async () => {
+        const log = document.getElementById('login') as HTMLFormElement;
+        const reg = document.getElementById('register') as HTMLFormElement;
+        const btn = document.getElementById('invisibleButton') as HTMLButtonElement;
+        const regButn = document.getElementById('regButton') as HTMLButtonElement;
+        const logButn = document.getElementById('logButton') as HTMLButtonElement;
+        log.style.display = "block"
+        reg.style.display = "none"
+        btn.style.left = "110px";
+        regButn.style.color = "green";
+        logButn.style.color = "black";
+    }
    
     render(){
         const { regEmail, regUsername, regName, regPassword} = this.state;
  
-        return <div>
+        return <div>      
             <body>
-                <h2 id="teamName">EasyWay Fitness</h2>
                 <div className="container SignUpForm">
+                <h2 id="teamName">EasyWay Fitness</h2>
                     <div className="col-lg-12">
                         <div className="container">
                             <div className="SignUpSignInButton">
-                                <span className="toggleButton">Regisztráció</span>
-                                <span className="toggleButton">Belépés</span>
+                                <div id="invisibleButton"></div>
+                                <button className="toggleButton" id="regButton" onClick={this.Registration}>Regisztráció</button>
+                                <button className="toggleButton" id="logButton" onClick={this.Login}>Belépés</button>
                             </div>
                             <br />
-                            <form id="" method="post" action="http://localhost:3000/register">
-                                <div className="container inputGroup">
+                            <form id="login" className="inputGroup" method="post" action="http://localhost:3000/register">
+                                <div className="container inputs">
                                     <label htmlFor="user_email"></label>
-                                    <input type="text" name="user_eamil" id="user_email" value={regEmail} onChange={e=> this.setState({regEmail: e.currentTarget.value})} placeholder="Email" required /> <br />
-                                    <label htmlFor="user_name"></label>
-                                    <input type="text" name="user_name" id="user_name" value={regUsername} onChange={e=> this.setState({regUsername: e.currentTarget.value})} placeholder="Felhasználónév" required /> <br />
-                                    <label htmlFor="name"></label>
-                                    <input type="text" name="name" id="name" value={regName} onChange={e=> this.setState({regName: e.currentTarget.value})} placeholder="Teljes név" required /> <br />
+                                    <input type="text" name="user_eamil" className="inputField" value={regEmail} onChange={e=> this.setState({regEmail: e.currentTarget.value})} placeholder="Email" required /> <br />
                                     <label htmlFor="user_password"></label>
-                                    <input type="password" name="user_password" id="user_password" value={regPassword} onChange={e=> this.setState({regPassword: e.currentTarget.value})} placeholder="jelszó" required /><br />
+                                    <input type="password" name="user_password" className="inputField" value={regPassword} onChange={e=> this.setState({regPassword: e.currentTarget.value})} placeholder="jelszó" required /><br />
+                                </div>
+                                <button className="btn btn-success" onClick={this.handlerRegister}>Belépés</button>
+                            </form>
+                            <form id="register" className="inputGroup" method="post" action="http://localhost:3000/register">
+                                <div className="container inputs">
+                                    <label htmlFor="user_email"></label>
+                                    <input type="text" name="user_eamil" className="inputField" value={regEmail} onChange={e=> this.setState({regEmail: e.currentTarget.value})} placeholder="Email" required /> <br />
+                                    <label htmlFor="user_name"></label>
+                                    <input type="text" name="user_name" className="inputField" value={regUsername} onChange={e=> this.setState({regUsername: e.currentTarget.value})} placeholder="Felhasználónév" required /> <br />
+                                    <label htmlFor="name"></label>
+                                    <input type="text" name="name" className="inputField" value={regName} onChange={e=> this.setState({regName: e.currentTarget.value})} placeholder="Teljes név" required /> <br />
+                                    <label htmlFor="user_password"></label>
+                                    <input type="password" name="user_password" className="inputField" value={regPassword} onChange={e=> this.setState({regPassword: e.currentTarget.value})} placeholder="jelszó" required /><br />
                                 </div>
                                 <button className="btn btn-success" onClick={this.handlerRegister}>Regisztráció</button>
                             </form>
