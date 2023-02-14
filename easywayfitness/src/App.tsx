3a32366c7512538cfcb327a6e2385c610fb7d6a7
Diak <diak@petrik>
2023-02-14 14:57:10 +0100

    fooldal elkezdve

--- easywayfitness/src/App.tsx
+++ easywayfitness/src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component<{}>{
+  render() {
+    return <div>
+      <div className='body'> 
+        <div className='container'>
+          <nav>
+            <span id='teamName'>EasyWay Fitness</span>
+              <ul className='nav justify-content-center'>
+                <li><a href='#'>Főoldal</a></li>
+                <li><a href='#'>Ismeretterjesztők</a></li>
+                <li><a href='#'>Gyakorlatok</a></li>
+                <li><a href='#'>Kalkulátor</a></li>
+              </ul>
+                <button id='registration'>Regisztráció</button>
+            </nav>
+            
+            <div className='container findTheWay'>
+              <h3>Találd meg az utat</h3>
+              <h3>A testi tökéletesség felé</h3>
+              <hr />
+              <span>Itt megtanulhatod az alapokat</span><br />
+              <span>Segítünk az elindulásban</span><br />
+              <button id='go'>Mehet</button>
+            </div>
+
+            <div className='container educators'>
+              <h3>Ismeretterjesztők</h3>
+              <div className='container'>
+                <div className='row'>
+                  <div className='col-lg-1'>
+                   
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+<br /><br /><b><br /><br />adfeafw</b>
+          </div>
+      </div>
     </div>
-  );
+  }
 }
 
 export default App;
