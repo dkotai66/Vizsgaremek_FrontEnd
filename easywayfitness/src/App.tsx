5aa4fd3b58ea908470c0e4fb36a7e191520a476f
Kótai Dániel <dkotai66@gmail.com>
2023-02-15 11:35:18 +0100

    fooldal kep hozzaadva, leiras terulete megcsinalva, legordulo menu megcsinalva

--- easywayfitness/src/App.tsx
+++ easywayfitness/src/App.tsx
@@ -17,24 +17,79 @@ class App extends Component<{}>{
                 <button id='registration'>Regisztráció</button>
             </nav>
             
-            <div className='container findTheWay'>
-              <h3>Találd meg az utat</h3>
-              <h3>A testi tökéletesség felé</h3>
-              <hr />
-              <span>Itt megtanulhatod az alapokat</span><br />
-              <span>Segítünk az elindulásban</span><br />
-              <button id='go'>Mehet</button>
+            <div className='findTheWay'>
+              <div className='findTheWayContent'>
+                <h2>Találd meg az utat</h2>
+                  <h2>A testi tökéletesség felé</h2>
+                  <hr />
+                  <span>Itt megtanulhatod az alapokat</span><br />
+                  <span>Segítünk az elindulásban</span><br />
+                  <button id='go'>Mehet</button>
+              </div>
+              <div className='findTheWayImage'>
+                <img src="./img/pullup.jpg" alt="pullupimage" id='pullupImage'/>
+              </div>   
             </div>
 
             <div className='container educators'>
               <h3>Ismeretterjesztők</h3>
-              <div className='container'>
                 <div className='row'>
-                  <div className='col-lg-1'>
-                   
+                  <div className='col-lg-2 dropdown'>
+                   <p>Fehérjék</p>
+                   <div className='dropdownContent'>
+                    <p>Tejsavú fehérjék</p>
+                    <p>Fehérje izolátum</p>
+                    <p>Hidrolizált fehérjék</p>
+                    <p>Növényalapú fehérjék</p>
+                    <p>Állatifehérje</p>
+                   </div>
+                  </div>
+                  <div className='col-lg-2 dropdown'>
+                   <p>Aminosavak</p>
+                   <div className='dropdownContent'>
+                    <p>BCAA</p>
+                    <p>L-Glutamin</p>
+                    <p>EAA</p>
+                    <p>Arginin</p>
+                   </div>
+                  </div>
+                  <div className='col-lg-2 dropdown'>
+                   <p>Kreatin</p>
+                   <div className='dropdownContent'>
+                    <p>Kreatin-monohidrát</p>
+                   </div>
+                  </div>
+                  <div className='col-lg-2 dropdown'>
+                   <p>Tömegnövelőszerek</p>
+                  </div>
+                  <div className='col-lg-2 dropdown'>
+                   <p>Vitaminok</p>
+                   <div className='dropdownContent'>
+                    <p>Multivitaminok</p>
+                    <p>A-vitamin</p>
+                    <p>B-vitamin</p>
+                    <p>C-vitamin</p>
+                    <p>D-vitamin</p>
+                    <p>E-vitamin</p>
+                    <p>F-vitamin</p>
+                    <p>K-vitamin</p>
+                   </div>
+                  </div>
+                  <div className='col-lg-2 dropdown'>
+                   <p>Izületvédelem</p>
+                   <div className='dropdownContent'>
+                    <p>Kollagén</p>
+                    <p>Glkükózamin</p>
+                   </div>
                   </div>
                 </div>
-              </div>
+                <div className='container contentInformation'>
+                  <div className='row tejsavo'>
+                    <div className='col-lg-12'>
+                      A tejsavó fehérjét tejből technológiai eljárásokkal állítják elő. A tejsavó fehérje porok mindegyiké kissé eltér az összetevők emészthetőségében, emésztési sebességében, és persze az árában. Ezért célnak és igénynek megfelelően lehet választani tejsavó koncentrátumom, izolátumot, hidrolizátumot.
+                    </div>
+                  </div>
+                </div>
             </div>
 
 
