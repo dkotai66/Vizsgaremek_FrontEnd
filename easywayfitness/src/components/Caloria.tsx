a153e3c2d5a93186b886c7c0182e2f981df1d45e
dani <dkotai66@gmail.com>
2023-03-16 15:15:24 +0100

    Kalória adatok lekérdezése, kiválasztása, kiiratása működik

--- easywayfitness/src/components/Caloria.tsx
+++ easywayfitness/src/components/Caloria.tsx
@@ -4,6 +4,8 @@ import './Caloria.css'
 
 interface State {
     etelek: Etel[];
+    currentFood: Etel;
+    currentCalorie: number;
 }
 
 interface Etel {
@@ -25,10 +27,12 @@ export default class Caloria extends Component<{}, State> {
         
         this.state = {
           etelek: [],
+          currentFood: {name: '', calorie: 0, protein: 0, carbohydrate: 0, fat: 0} as Etel,
+          currentCalorie: 0,
         }
       }
 
-    async ekszerekBetoltese(){
+    async etelekBetoltese(){
         let response = await fetch('http://localhost:3000/food');
         let data = await response.json() as Etel[];
         this.setState({
@@ -37,7 +41,7 @@ export default class Caloria extends Component<{}, State> {
       }
 
     componentDidMount(){
-        this.ekszerekBetoltese();
+        this.etelekBetoltese();
     }
 
     render() {
@@ -56,15 +60,21 @@ export default class Caloria extends Component<{}, State> {
                             <button id='registration'><NavLink to='/SignUpSignIn' id='signupsignin'>Regisztráció</NavLink></button>
                         </nav>
                     </header>
-                    <div className="caloriaContainer">
+                    <div className="caloriaContainer"> 
                         <h2>Add meg mit ettél</h2>
-                        <select name="food">
+                        <select name="food" onChange={a=> this.setState({currentFood: this.state.etelek[parseInt(a.currentTarget.value)]})} defaultValue='nothing'>
+                            <option value="nothing" hidden>----</option>
                             {
-                                this.state.etelek.map(etel=><option value={etel.name}>{etel.name}</option>)
+                                this.state.etelek.map((etel, index)=><option value={index}>{etel.name}</option>)
                             }
                         </select>
-                        <p>Az étel neve: </p>
-                        <p></p>
+                        
+
+                        <p>Az étel neve: {this.state.currentFood.name} </p>
+                        <p>Az étel kalória mennyisége: {this.state.currentFood.calorie} <small>Kiválaszott étel 100 grammjára vonatkozik</small></p>
+                        <p>Az étel fehérje mennyisége: {this.state.currentFood.protein} <small>Kiválaszott étel 100 grammjára vonatkozik</small></p>
+                        <p>Az étel szénhidrát mennyisége: {this.state.currentFood.carbohydrate} <small>Kiválaszott étel 100 grammjára vonatkozik</small></p>
+                        <p>Az étel zsír mennyisége: {this.state.currentFood.fat} <small>Kiválaszott étel 100 grammjára vonatkozik</small></p>
                     </div>
                 </div>
             </body>
