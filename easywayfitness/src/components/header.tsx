ab42b18955e82c49a83dea459371ef0104c288b3
Kótai Dániel <dkotai66@gmail.com>
2023-03-27 16:15:32 +0200

    navbar bootstrappel kiegeszitve

--- easywayfitness/src/components/header.tsx
+++ easywayfitness/src/components/header.tsx
@@ -1,19 +1,28 @@
-import { Component } from "react";
+import { Component} from "react";
 import { NavLink } from "react-router-dom";
+import { GoThreeBars } from "react-icons/go"; 
+import "./header.css"
 
 export default class Header extends Component {
+
     render() {
         return <div>
             <header>
-                <nav>
-                <span id='teamName'>EasyWay Fitness</span>
-                <ul className='nav justify-content-center'>
-                    <li><NavLink to='/'>Főoldal</NavLink></li>
-                    <li><NavLink to='/FoodCaloria'>Kalóriák</NavLink></li>
-                    <li><a href='/Record'>Naplózás</a></li>
-                    <li><NavLink to='/Calculator'>Kalkulátor</NavLink></li>
-                </ul>
-                    <button id='registration'><NavLink to='/SignUpSignIn' id='signupsignin'>Regisztráció</NavLink></button>
+                <nav className="navbar navbar-expand-sm  navbar-dark ">
+                    <a id='teamName' className="navbar-brand">EasyWay Fitness</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul className="navbar-nav">
+                        <li className="nav-item"><NavLink to='/' className="nav-link">Főoldal</NavLink></li>
+                        <li className="nav-item"><NavLink to='/FoodCaloria' className="nav-link">Kalóriák</NavLink></li>
+                        <li className="nav-item"><NavLink to='/Record' className="nav-link">Naplózás</NavLink></li>
+                        <li className="nav-item"><NavLink to='/Calculator' className="nav-link">Kalkulátor</NavLink></li>
+                    </ul>
+                    <button className="nav-item" id='registration'><NavLink to='/SignUpSignIn' className="nav-link" id='signupsignin'>Regisztráció</NavLink></button>
+                    </div>
                 </nav>
             </header>
         </div>
